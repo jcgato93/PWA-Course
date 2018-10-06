cd92b6fcc6d11973ba9060c34ffc626e7576e090
jcgato93 <npgcamilo@gmail.com>
2018-10-06 17:16:04 -0500

    Gestion de versiones del SW

--- pwa/src/app/app.component.ts
+++ pwa/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  constructor(private swUpdate: SwUpdate){
+
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    
+    //Check if Service Worker is available for the browser
+    if (this.swUpdate.isEnabled) {
+      //Check if exist a new version of services worker
+      this.swUpdate.available.subscribe(()=>{
+        //refresh the page
+        window.location.reload();
+      });
+    }
+    
+  }
+
 }

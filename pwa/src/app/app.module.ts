04a6e324a3c0c4f61a9ab3571ec15c8e90f584bd
Camilo.IT <npgcamilo@gmail.com>
2018-10-08 16:47:25 -0500

    Implementacion de Firebase

--- pwa/src/app/app.module.ts
+++ pwa/src/app/app.module.ts
@@ -7,6 +7,13 @@ import { environment } from '../environments/environment';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -15,7 +22,12 @@ import { MaterialModule } from './material/material.module';
     BrowserModule,
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
+    AngularFireStorageModule, // imports firebase/storage only needed for storage features
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- pwa/src/environments/environment.ts
+++ pwa/src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyDFM8C9l_sreg-3-59_WMl6N80EhCi1PX4",
+    authDomain: "notapps-34526.firebaseapp.com",
+    databaseURL: "https://notapps-34526.firebaseio.com",
+    projectId: "notapps-34526",
+    storageBucket: "",
+    messagingSenderId: "61516866892"
+  }
+  
+
+
 };
 
 /*

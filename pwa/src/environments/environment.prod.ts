9ff9156fa3d69a4d8eeb11458f0ae65d1ca8240c
Camilo.IT <npgcamilo@gmail.com>
2018-10-11 18:00:19 -0500

    add material design scss

--- pwa/src/environments/environment.prod.ts
+++ pwa/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: 'AIzaSyDFM8C9l_sreg-3-59_WMl6N80EhCi1PX4',
+    authDomain: 'notapps-34526.firebaseapp.com',
+    databaseURL: 'https://notapps-34526.firebaseio.com',
+    projectId: 'notapps-34526',
+    storageBucket: '',
+    messagingSenderId: '61516866892'
+  }
 };
